364f49b67bc67a4efda415d6653482d8c940b694
MatijaSabo <matijasaboli37@gmail.com>
2016-11-04 11:26:04 +0100

    Meni - korisnik
    Napravljen glavni meni aplikacije za korisnika

--- ZlatnaRezervacija/app/src/main/java/com/example/matija/zlatnarezervacija/MainActivity.java
+++ ZlatnaRezervacija/app/src/main/java/com/example/matija/zlatnarezervacija/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.matija.zlatnarezervacija;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_login) Button mainButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +23,13 @@ public class MainActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_login)
+    public void mainButtonClick(View view){
+        Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+        startActivity(intent);
     }
 }
---
+++ ZlatnaRezervacija/app/src/main/java/com/example/matija/zlatnarezervacija/MenuActivity.java
@@ -0,0 +1,39 @@
+package com.example.matija.zlatnarezervacija;
+
+import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MenuActivity extends AppCompatActivity {
+
+    ActionBarDrawerToggle toggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.activity_menu);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(toggle.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
+}
