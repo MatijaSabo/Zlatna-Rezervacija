cd869c53861c2599958b97460a89668e90306757
MatijaSabo <matijasaboli37@gmail.com>
2017-01-19 23:31:29 +0100

    Dizajn dorade
    Redizajn aktivnosti koja prikazuje rezervaciju na čekanju
    Napravljen dizajn aktivnosti koja prikazuje zahtjev za otkazivanje
    rezervacije

--- ZlatnaRezervacija/app/src/main/java/com/foi/air/zlatnarezervacija/RequestForCancelActivity.java
+++ ZlatnaRezervacija/app/src/main/java/com/foi/air/zlatnarezervacija/RequestForCancelActivity.java
@@ -1,12 +1,24 @@
 package com.foi.air.zlatnarezervacija;
 
+import android.app.ProgressDialog;
+import android.net.ConnectivityManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class RequestForCancelActivity extends AppCompatActivity {
+import com.foi.webservice.data_loaders.DataLoadedListener;
+import com.foi.webservice.data_loaders.DataLoader;
+
+public class RequestForCancelActivity extends AppCompatActivity implements DataLoadedListener {
 
     private String reservation_intent;
+    ProgressDialog progress;
+
+    TextView user, date, time_arrival, time_checkout, persons, melas, remark, description;
+    Button odbij, otkazi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +28,20 @@ public class RequestForCancelActivity extends AppCompatActivity {
         reservation_intent = getIntent().getStringExtra("reservation_id");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Rezervacija " + reservation_intent);
+
+        user = (TextView) findViewById(R.id.activity_request_for_cancel_user);
+        date = (TextView) findViewById(R.id.activity_request_for_cancel_date);
+        time_arrival = (TextView) findViewById(R.id.activity_request_for_cancel_time_arrival);
+        time_checkout = (TextView) findViewById(R.id.activity_request_for_cancel_time_checkout);
+        persons = (TextView) findViewById(R.id.activity_request_for_cancel_persons);
+        melas = (TextView) findViewById(R.id.activity_request_for_cancel_meals);
+        remark = (TextView) findViewById(R.id.activity_request_for_cancel_remark);
+        description = (TextView) findViewById(R.id.activity_request_for_cancel_description);
+
+        odbij = (Button) findViewById(R.id.btn_odbij_zahtjev_za_otkazivanje);
+        otkazi = (Button) findViewById(R.id.btn_potvrdi_zahtjev_za_otkazivanje);
+
+        getReservationData();
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -26,5 +52,22 @@ public class RequestForCancelActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void getReservationData() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(this.CONNECTIVITY_SERVICE);
+        if (cm.getActiveNetworkInfo() != null) {
+            //progress = ProgressDialog.show(this, getString(R.string.FetchingData), getString(R.string.PleaseWait));
+            //DataLoader dataLoader1;
+            //dataLoader1 = new WsReservationOnHold();
+            //dataLoader1.loadReservationOnHold(this, reservation_intent.toString());
+        } else {
+            Toast.makeText(this, R.string.NoInternet, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onDataLoaded(Object result) {
+        progress.dismiss();
     }
 }
