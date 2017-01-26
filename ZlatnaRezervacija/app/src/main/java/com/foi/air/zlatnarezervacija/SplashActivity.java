79d166b6fac037b5ed5782ef5df0e61597577e1d
MatijaSabo <matijasaboli37@gmail.com>
2017-01-26 23:46:13 +0100

    Splash screen
    Napravljen splash screen aplikacije

---
+++ ZlatnaRezervacija/app/src/main/java/com/foi/air/zlatnarezervacija/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.foi.air.zlatnarezervacija;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Matija on 26.1.2017..
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
