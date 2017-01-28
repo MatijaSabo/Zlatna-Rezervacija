26c577121b19a97b5f0324e61b1f0277c8e3c4a0
Lovro <ljakopov@foi.hr>
2017-01-28 16:41:50 +0100

    Dodan je opis restorana sa slikama
    Kreirane su tri kartice sa slikom restorana i posebnim
    ExpandableTextView. On je dodan u projekt ko posebna klasa. Dodane su
    također ikone koje služe za ljepši ispis.

---
+++ ZlatnaRezervacija/app/src/main/java/com/foi/air/zlatnarezervacija/TextView/ExpandableTextView.java
@@ -0,0 +1,82 @@
+package com.foi.air.zlatnarezervacija.TextView;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
+import com.foi.air.zlatnarezervacija.R;
+
+/**
+ * Created by Lovro on 28.1.2017..
+ */
+
+public class ExpandableTextView extends TextView implements View.OnClickListener{
+
+    private static final int MAX_LINES = 5;
+    private int currentMaxLines = Integer.MAX_VALUE;
+    private boolean flag=false;
+
+
+    public ExpandableTextView(Context context)
+    {
+        super(context);
+        setOnClickListener(this);
+    }
+    public ExpandableTextView(Context context, AttributeSet attrs, int defStyle)
+    {
+        super(context, attrs, defStyle);
+        setOnClickListener(this);
+    }
+
+    public ExpandableTextView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        setOnClickListener(this);
+    }
+
+    @Override
+    protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter)
+    {
+        post(new Runnable()
+        {
+            public void run()
+            {
+                if (getLineCount()>MAX_LINES)
+                {
+                    setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, R.mipmap.ic_keyboard_arrow_down_black_18dp);
+                    flag=true;
+                }
+                else
+                    setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+
+                setMaxLines(MAX_LINES);
+            }
+        });
+    }
+
+    @Override
+    public void setMaxLines(int maxLines)
+    {
+        currentMaxLines = maxLines;
+        super.setMaxLines(maxLines);
+    }
+
+    public int getMyMaxLines()
+    {
+        return currentMaxLines;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (flag == true) {
+            if (getMyMaxLines() == Integer.MAX_VALUE) {
+                setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, R.mipmap.ic_keyboard_arrow_down_black_18dp);
+                setMaxLines(MAX_LINES);
+            } else {
+                setMaxLines(Integer.MAX_VALUE);
+                setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, R.mipmap.ic_keyboard_arrow_up_black_18dp);
+            }
+        }
+    }
+}
